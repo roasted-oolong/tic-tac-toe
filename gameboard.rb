f21660c1e179ef03dd0330f267f32958120db934
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-27 20:38:40 +0000

    Add concept for update_board, check_winner, board_full?
    The above methods have been added to the Gameboard with their intended functions,
    but they have not yet been coded into actual features.
    This is to understand the whole picture without losing progress

--- gameboard.rb
+++ gameboard.rb
@@ -8,6 +8,18 @@ class Gameboard
       puts row.map { |cell| cell || '_' }.join(' ') #display blank cells as '_' instead of nil
     end
   end
+
+  def update_board(position, symbol)
+    #update a cell with the player's symbol
+  end
+
+  def check_winner
+    #evaluate if there's a winner
+  end
+
+  def board_full?
+    #check if the board is full
+  end
 end
 gameboard = Gameboard.new
 gameboard.display_board
