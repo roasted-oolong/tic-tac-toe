0af59300cb9a318d769ed5ee7bb04aa65f4b8d04
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-27 21:23:12 +0000

    Added concept variables and methods
    Listed variables and methods that could go into this class.
    Purpose is to brainstorm and refine while saving progress

---
+++ game.rb
@@ -0,0 +1,22 @@
+class Game
+  #variables
+  @players
+  @gameboard
+  @current_player
+
+  def play_turn
+    #handle single turn (get player input, update board, check win)
+  end
+
+  def switch_player
+    #change current player
+  end
+
+  def game_over?
+    #check if game is over (win or draw)
+  end
+
+  def end_game
+    #display winner/loser
+  end
+end
