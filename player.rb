0f96d29f7d99e62fd30661d628b265b14960a7cc
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-27 21:17:57 +0000

    Add concept for make_move method
    Added as concept. Not sure if this should go under game class or player class

--- player.rb
+++ player.rb
@@ -1,8 +1,12 @@
 class Player
   attr_reader :name, :symbol
-  
+
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
+  end
+
+  def make_move
+    #get input from player
   end
 end
