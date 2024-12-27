282ee72375eeddd3ee6b3332f9846e7b6b2c14ab
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-12-27 21:04:13 +0000

    Finish winner? method
    Changed from check_winner method.
    Method for checking for the winner is privatized in other methods (i.e., check_rows, check_columns, etc.)

--- gameboard.rb
+++ gameboard.rb
@@ -1,4 +1,6 @@
 class Gameboard
+  attr_reader :board
+  
   def initialize
     @board = Array.new(3) { Array.new(3, nil) }
   end
@@ -19,15 +21,44 @@ class Gameboard
       true
     else
       false
+    end
   end
 
-  def check_winner
-    #evaluate if there's a winner
+  def winner?
+    check_rows || check_columns || check_diagonals
   end
 
   def board_full?
     #check if the board is full
   end
+
+  private
+  def check_rows
+    @board.each { |row| row[0] if winning_line?(row)}
+  end
+
+  def check_columns
+    (0...3).each do |column|
+      column = @board.map { |row| row[column] }
+    end
+  end
+
+  def check_diagonals
+    diaganol1 = (0...3).map { |i| @board[i][i] }
+    diagonal1[0] if winning_line?(diaganol1)
+
+    diagonal2 = (0...3).map { |i| @board[i][2- i] }
+    diagonal1[0] if winning_line?(diaganol2)
+
+    nil
+  end
+
+  def winning_line?(line)
+    line.all? { |cell| cell == line[0] && cell !=nil}
+  end
 end
 gameboard = Gameboard.new
 gameboard.display_board
+gameboard.board[0][0] = 'X'
+gameboard.board[0][1] = 'X'
+gameboard.board[0][2] = 'X'
