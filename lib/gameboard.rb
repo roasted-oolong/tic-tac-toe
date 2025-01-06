48c439dac1245b5112f4147a0991e385b6ae22b1
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2025-01-06 05:56:44 +0000

    Fix error in checking for winner?
    Problem: Logic for checking for the winner was not properly functioning, leading the game to continue until the board was full (a tie).
    Solution: Added return statement in row, column, and diagonal checks to ensure results returned properly.

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -35,24 +35,24 @@ class Gameboard
   end
 
   def check_rows
-    @board.each { |row| row[0] if winning_line?(row) }
+    @board.each { |row| return row[0] if winning_line?(row) }
     nil
   end
 
   def check_columns
-    (0...3).each do |column|
-      column = @board.map { |row| row[column] }
-      column[0] if winning_line?(column)
+    (0...3).each do |col|
+      column = @board.map { |row| row[col] }
+      return column[0] if winning_line?(column)
     end
     nil
   end
 
   def check_diagonals
     diagonal1 = (0...3).map { |i| @board[i][i] }
-    diagonal1[0] if winning_line?(diagonal1)
+    return diagonal1[0] if winning_line?(diagonal1)
 
     diagonal2 = (0...3).map { |i| @board[i][2 - i] }
-    diagonal2[0] if winning_line?(diagonal2)
+    return diagonal2[0] if winning_line?(diagonal2)
 
     nil
   end
